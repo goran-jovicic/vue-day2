db2494e66de359d791b66db441c6969ed167b03f
Mona Lisa <gugacns94@gmail.com>
2019-06-25 19:14:22 +0200

    zdatak 5

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,4 +1,22 @@
 <template>
+<div>
+    <form @submit.prevent="addAnimal"> 
+      <div>
+        <label for="vrsta">Vrsta : </label>
+        <input type="text" id="vrsta" v-model="newAnimal.vrsta" required/>
+      </div>
+      <div>
+        <label for="ime">Ime : </label>
+        <input type="text" id="ime" v-model="newAnimal.ime" required/>
+      </div>
+      <div>
+        <label for="datum_rodjenja">Datum rodjenja : </label>
+        <input type="date" id="datum_rodjenja" v-model="newAnimal.datum_rodjenja" required/>
+      </div>
+      <div>
+        <button type="submit">Add animal</button>
+      </div>
+    </form>
     <ul>
       <li v-for="(animal,index) in animals" :key="index">
         Vrsta : {{ animal.vrsta }} , ime : {{ animal.ime}} , datum rodjenja : {{ animal.datum_rodjenja ? animal.datum_rodjenja : 'Nepoznat' }}
@@ -6,6 +24,7 @@
         <button @click="moveToTop(index)"> Move to top </button>
       </li>
     </ul>
+    </div>
 </template>
 
 <script>
@@ -17,6 +36,8 @@ export default {
 
   data (){
     return {
+      newAnimal: this.getDefault(),
+
       animals: [
         { vrsta: 'pas', ime: 'Boni', datum_rodjenja: '24-09-2005' },
         { vrsta: 'slon', ime: 'Ganesh', datum_rodjenja: '06-06-2006'},
@@ -36,6 +57,19 @@ export default {
       let animalCopy = this.animals[index]
       this.animals.splice(index,1)
       this.animals.unshift(animalCopy) 
+    },
+
+    addAnimal(){
+      this.animals.push({...this.newAnimal})
+      this.newAnimal = this.getDefault()
+    },
+
+    getDefault () {
+      return {
+        vrsta: '',
+        ime: '',
+        datum_rodjenja: ''
+      }
     }
   }
 }
